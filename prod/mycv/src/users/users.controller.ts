e120f20b8f7455110558ecfe53b7aede18af4060
kimerum333 <kimerum333@gmail.com>
2025-05-24 10:31:00 +0900

    커스텀인터셉터-직렬화해결

--- prod/mycv/src/users/users.controller.ts
+++ prod/mycv/src/users/users.controller.ts
@@ -8,7 +8,10 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
     constructor(private usersService: UsersService) {
@@ -20,9 +23,10 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
-    @UseInterceptors(ClassSerializerInterceptor )
+
     @Get('/:id')
     async findUser(@Param('id') id: string) {
+
         const user = await this.usersService.findOne(parseInt(id, 10));
         if (!user) {
             throw new NotFoundException('user not found!');
